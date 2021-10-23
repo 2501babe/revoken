f9f5d165a8dd3ef2830355957f1eb08d5a9c826c
hanako mumei <81144685+2501babe@users.noreply.github.com>
2021-10-23 09:37:21 +0000

    explain wtf this is

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,6 @@
             <option>fortuna</option>
         </select>
 
-        <!-- ill use the wallet adapter if someone tells me how to make the ui stuff look like 1995 -->
         <span v-if="walletConnected">
             <button @click="wallet.disconnect()">disconnect</button>
         </span>
@@ -28,6 +27,47 @@
     </div>
 
     <h1>solana token revoken</h1>
+
+    <div>
+        <p>
+            the spl token program has an instruction called approve.
+            approve is intended to function similar the ierc20 function of the same name:
+            instead of signing a token account away to a program to modify as it pleases,
+            you independently grant it a maximum budget it can use without further authorization
+        </p>
+
+        <div class="center"><img src="/hana-tweet.png"/></div>
+
+        <p>
+            but thanks to how cross program calling works in solana this can actually be used against you.
+            if you sign for a token account to a malicious program, it can tag you with an approve instruction
+            and withdraw funds at an arbitrary later date
+        </p>
+
+        <p>
+            to be clear it could also just steal your tokens then and there.
+            but a program may want to use approvals a) to fly under the radar for some time to massively increase its take,
+            and b) to fool phantoms new balance change simulator by not actually changing balances in the transaction you sign.
+            this would be a really cool attack and would scare a lot of people i think
+        </p>
+
+        <p>
+            this is all because of another interesting ("interesting") difference between solana and ethereum.
+            on ethereum, erc20 transfer always uses msg.sender as the token sender.
+            it is not possible for a contract to avoid the two legged approve and  transferFrom flow.
+            but on solana, theres only transfer. gated by either a signature by the wallet pubkey or an approval
+            cross program calls pass the hash so to speak, and programs cannot distinguish an intermediary from an origin
+        </p>
+
+        <p>
+            anyway!! this is a clientside app that pulls your token accounts, checks for active approvals, 
+            and creates and sends transactions to revoke them at your pleasure
+        </p>
+
+        <div class="center">
+            <button :disabled="!walletConnected">lets do it</button>
+        </div>
+    </div>
 
     <p class="center"><a href="/index.html">home</a></p>
 </template>
@@ -101,8 +141,10 @@
                 this.connection = new w3.Connection(NETWORKS[this.selectedNetwork]);
             },
             async fetchBalance() {
-                let bal = await this.connection.getBalance(this.wallet.publicKey);
-                this.solBalance = (bal / 10 ** 9).toFixed(3);
+                this.solBalance = await
+                    this.connection.getBalance(this.wallet.publicKey)
+                    .then(bal => (bal / 10 ** 9).toFixed(3))
+                    .catch(() => null);
             }
         }
     }
