dec88efe42c461361dcceaae888dc0c4dac408d1
hanako mumei <81144685+2501babe@users.noreply.github.com>
2021-10-27 10:10:32 +0000

    wire sollet and phantom in again

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -11,7 +11,7 @@
         <button @click="wallet.disconnect()">disconnect</button>
     </span>
     <span v-else>
-        <button @click="connectWallet2()">connect</button>
+        <button @click="connectWallet()">connect</button>
     </span>
 
     <div>
@@ -19,10 +19,10 @@
             <Address :pubkey="wallet.publicKey"/>| {{ solBalance == null ? "--" : solBalance }} sol
         </span>
         <span v-else>
-            <input type="radio" id="sollet-rad" value="Sollet" v-model="walletChoice"/>
-            <label for="sollet-rad">sollet</label>
-            <input type="radio" id="phantom-rad" value="Phantom" v-model="walletChoice" :disabled="!phantom"/>
-            <label for="phantom-rad" :class="{'label-disabled': !phantom}">phantom</label>
+            <input type="radio" id="sollet-rad" value="sollet" v-model="walletChoice"/>
+            <label for="sollet-rad">sollet (this probably doesnt work)</label>
+            <input type="radio" id="phantom-rad" value="phantom" v-model="walletChoice"/>
+            <label for="phantom-rad">phantom</label>
         </span>
         <br/>
     </div>
@@ -131,8 +131,6 @@
 <script>
 import * as w3 from "@solana/web3.js";
 import { TOKEN_PROGRAM_ID, Token, AccountLayout, u64 } from "@solana/spl-token";
-//import Wallet from "../../node_modules/@project-serum/sol-wallet-adapter";
-//import { useWallet } from "@solana/wallet-adapter-vue";
 import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom";
 import { SolletWalletAdapter } from "@solana/wallet-adapter-sollet";
 import Dots from "./Dots.vue";
@@ -153,37 +151,6 @@ export default {
     },
     mounted() {
         let vm = window.revoken = this;
-
-        // XXX ok how am i doing this fucking refactor
-        // usewallet gives me the adapter and select which is all i need
-        // when a user presses connect we call connectwallet which:
-        // * selects the chosen wallet
-        // * attaches the event handlers
-        // * connects
-
-        vm.wallet = new SolletWalletAdapter({ provider: "https://www.sollet.io", network: "mainnet-beta" });
-        console.log("HANA wallet:", vm.wallet);
-
-        //let { select, adapter } = useWallet();
-        //vm.selectWallet = select;
-        //vm.wallet = adapter;
-
-/*
-        // phantom loads slower than the page so we have to wait for it
-        // i dunno if other wallets use this name but just in case
-        let loop = setInterval(() => {
-            if(window.solana) {
-                if(window.solana.isPhantom) {
-                    vm.phantom = window.solana;
-                } else {
-                    vm.phantom = false;
-                }
-
-                clearInterval(loop);
-            }
-        }, 100);
-*/
-
         vm.connectChain();
     },
     data() {
@@ -191,14 +158,10 @@ export default {
             connection: null,
             wallet: null,
             walletConnected: false,
-            walletChoice: "Phantom",
-            phantom: null,
+            walletChoice: "phantom",
             solBalance: null,
             selectedNetwork: "mainnet-beta",
             tokens: null,
-            // XXX new mode here
-            selectWallet: null,
-            wtf: null,
         }
     },
     watch: {
@@ -209,34 +172,15 @@ export default {
         }
     },
     methods: {
-        async connectWallet2() {
-            let vm = this;
-
-            vm.wallet.on("connect", () => {
-                console.log("HANA connecting!");
-                vm.walletConnected = true;
-                vm.fetchBalance();
-            });
-
-            vm.wallet.on("disconnect", () => {
-                console.log("HANA disconnecting!");
-                vm.walletConnected = false;
-                vm.solBalance = null;
-                vm.tokens = null;
-            });
-
-            await vm.wallet.connect();
-        },
         async connectWallet() {
             let vm = this;
 
             switch(vm.walletChoice) {
                 case "sollet":
-                    //vm.wallet = await new Wallet("https://www.sollet.io");
+                    vm.wallet = new SolletWalletAdapter({provider: "https://www.sollet.io", network: vm.selectedNetwork});
                     break;
                 case "phantom":
-                    // this is safe because we block phantom from being selected until it loads
-                    vm.wallet = vm.phantom;
+                    vm.wallet = new PhantomWalletAdapter();
                     break;
                 default:
                     console.error("missed a case", this.walletChoice);
@@ -256,8 +200,6 @@ export default {
             });
 
             await vm.wallet.connect();
-
-            console.log(vm.wallet);
         },
         connectChain() {
             this.connection = new w3.Connection(NETWORKS[this.selectedNetwork]);
@@ -383,10 +325,6 @@ button, select {
 
 button:disabled, select:disabled {
     cursor: not-allowed;
-}
-
-.label-disabled {
-    opacity: 0.5;
 }
 
 .button-swap {
